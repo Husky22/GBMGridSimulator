ef81ecbd0189d6bee30675bfcda57dbad56a8cb4
Husky22 <niklas.v.minckwitz@googlemail.com>
2019-11-19 18:41:56 +0100

    path correction

--- testrun_trig.py
+++ testrun_trig.py
@@ -13,7 +13,7 @@ warnings.simplefilter('ignore')
 n_objects=1
 spectrumgrid=[1,1]
 #trigfile="rawdata/191017391/glg_trigdat_all_bn191017391_v01.fit"
-trigdat=glob("/rawdata/131229277/glg_trigdat_all_bn131229277_v0*.fit")[0]
+trigdat=glob("rawdata/131229277/glg_trigdat_all_bn131229277_v0*.fit")[0]
 simulation=Simulator(n_objects,spectrumgrid,trigdat)
 det_list=['n0','n1','n2','n3','n4','n5','n6','n7','n8','n9','na','nb','b0','b1']
 
