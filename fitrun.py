183318326a6ade6fa83ca0695c6b9a695437edd1
Husky22 <niklas.v.minckwitz@googlemail.com>
2019-12-05 16:20:06 +0100

    bugfix

--- fitrun.py
+++ fitrun.py
@@ -7,7 +7,7 @@ simplefilter("ignore")
 comm=MPI.COMM_WORLD
 rank=comm.Get_rank()
 envpath=os.environ.get('SIMULATOR')
-simulation=Simulator(2,[1,1],envpath+"/GBMGridSimulator/rawdata/191017391/glg_trigdat_all_bn191017391_v01.fit")
+simulation=Simulator(2,[1,1],envpath+"/rawdata/191017391/glg_trigdat_all_bn191017391_v01.fit")
 simulation.setup(K=1E-6)
 simulation.generate_j2000()
 if rank==0:
